45d7f64b92f88459712d7581a6760035b21ab494
vincent <vincentcharlebois@gmail.com>
2024-03-01 10:34:25 -0500

    Revert "Merge branch 'eduardoboucas-dev'"
    This reverts commit 18f110a2f60c3e4b264096f515493ef2e1457488, reversing
    changes made to 5d7ed7708775e3d4864382cca88d2d73ff875864.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <img src="logo.png" width="300">
 
-# Staticman [![coverage](https://img.shields.io/badge/coverage-81%25-yellow.svg?style=flat)](https://github.com/eduardoboucas/staticman) [![Build Status](https://travis-ci.org/eduardoboucas/staticman.svg?branch=master)](https://travis-ci.org/eduardoboucas/staticman) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-airbnb-brightgreen.svg)](https://github.com/airbnb/javascript)
+# Staticman [![coverage](https://img.shields.io/badge/coverage-81%25-yellow.svg?style=flat)](https://github.com/eduardoboucas/staticman) [![Build Status](https://travis-ci.org/eduardoboucas/staticman.svg?branch=master)](https://travis-ci.org/eduardoboucas/staticman) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 > Static sites with superpowers
 
@@ -16,13 +16,12 @@ You can download and run the Staticman API on your own infrastructure. The easie
 
 ## Requirements
 
-- Node.js 14.15.1+
+- Node.js 8.11.3+
 - npm
 - A [personal access token](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/) for the GitHub and/or GitLab account you want to run Staticman with
 - An RSA key in PEM format
 
 ## Setting up the server on your own infrastructure
-
 NOTE: The below steps are not required if deploying to Heroku. To deploy to Heroku, click the above deploy button and enter your configuration variables in the Heroku Dashboard.
 
 - Clone the repository and install the dependencies via npm.
@@ -36,7 +35,7 @@ NOTE: The below steps are not required if deploying to Heroku. To deploy to Hero
 - Create a development config file from the sample file.
 
   ```
-  cp config/sample.json config/development.json
+  cp config.sample.json config.development.json
   ```
 
 - Edit the newly-created config file with your GitHub and/or GitLab access token, SSH private key and the port to run the server. Click [here](https://staticman.net/docs/api) for the list of available configuration parameters.
@@ -58,14 +57,12 @@ Staticman runs as a bot using a GitHub and/or GitLab account, as opposed to acce
 To add Staticman to a repository, you need to add the bot as a collaborator with write access to the repository and ask the bot to accept the invite by firing a `GET` request to this URL:
 
 ```
-http://your-staticman-url/v3/connect/GITHUB-USERNAME/GITHUB-REPOSITORY
+http://your-staticman-url/v2/connect/GITHUB-USERNAME/GITHUB-REPOSITORY
 ```
-
-The full Staticman API documentation [can be found here](https://api.staticman.net/api-docs).
 
 ## Site configuration
 
-Staticman will look for a config file. For the deprecated `v1` endpoints, this is a `_config.yml` with a `staticman` property inside; for `v2` endpoints, Staticman looks for a `staticman.yml` file at the root of the repository.
+Staticman will look for a config file. For the deprecated `v1` endpoints, this is a  `_config.yml` with a `staticman` property inside; for `v2` endpoints, Staticman looks for a `staticman.yml` file at the root of the repository.
 
 For a list of available configuration parameters, please refer to the [documentation page](https://staticman.net/docs/configuration).
 
@@ -75,8 +72,6 @@ Would you like to contribute to Staticman? That's great! Here's how:
 
 1. Read the [contributing guidelines](CONTRIBUTING.md)
 1. Pull the repository and start hacking
-1. Run the dev server with `npm start-dev`
-1. Generated API docs are accessible at the `/api-docs` endpoint
 1. Make sure tests are passing by running `npm test`
 1. Send a pull request and celebrate
 
--- test/acceptance/api.test.js
+++ test/acceptance/api.test.js
@@ -1,116 +1,114 @@
-import nock from 'nock';
-import querystring from 'querystring';
+const config = require('../../config')
+const githubToken = config.get('githubToken')
+const helpers = require('../helpers')
+const nock = require('nock')
+const querystring = require('querystring')
+const request = helpers.wrappedRequest
+const sampleData = require('../helpers/sampleData')
+const StaticmanAPI = require('../../server')
 
-import config from '../../source/config';
-import * as helpers from '../helpers';
-import * as sampleData from '../helpers/sampleData';
-import StaticmanAPI from '../../source/server';
+const btoa = contents => Buffer.from(contents).toString('base64')
 
-const githubToken = config.get('githubToken');
-const request = helpers.wrappedRequest;
+let server
 
-const btoa = (contents) => Buffer.from(contents).toString('base64');
+beforeAll(done => {
+  server = new StaticmanAPI()
 
-let server;
+  server.start(() => {})
 
-beforeAll((done) => {
-  server = new StaticmanAPI();
+  done()
+})
 
-  server.start(() => {});
+afterAll(done => {
+  server.close()
 
-  done();
-});
-
-afterAll((done) => {
-  server.close();
-
-  done();
-});
+  done()
+})
 
 describe('Connect endpoint', () => {
-  test('accepts the invitation if one is found and replies with "Staticman connected!"', async () => {
-    const invitationId = 123;
+  test('accepts the invitation if one is found and replies with "OK!"', async () => {
+    const invitationId = 123
 
     const reqListInvititations = nock('https://api.github.com', {
       reqheaders: {
-        authorization: `token ${githubToken}`,
-      },
+        authorization: `token ${githubToken}`
+      }
     })
       .get('/user/repository_invitations')
       .reply(200, [
         {
           id: invitationId,
           repository: {
-            full_name: `johndoe/foobar`,
-          },
-        },
-      ]);
+            full_name: `johndoe/foobar`
+          }
+        }
+      ])
 
     const reqAcceptInvitation = nock('https://api.github.com', {
       reqheaders: {
-        authorization: `token ${githubToken}`,
-      },
+        authorization: `token ${githubToken}`
+      }
     })
       .patch(`/user/repository_invitations/${invitationId}`)
-      .reply(204);
+      .reply(204)
 
-    const response = await request('/v2/connect/johndoe/foobar');
-    expect(reqListInvititations.isDone()).toBe(true);
-    expect(reqAcceptInvitation.isDone()).toBe(true);
-    expect(response).toBe('Staticman connected!');
-  });
+    let response = await request('/v2/connect/johndoe/foobar')
+    expect(reqListInvititations.isDone()).toBe(true)
+    expect(reqAcceptInvitation.isDone()).toBe(true)
+    expect(response).toBe('OK!')
+  })
 
-  test('returns a 404 and an error message if a matching invitation is not found', () => {
+  test('returns a 404 and an error message if a matching invitation is not found', async () => {
     const invitationId = 123
     const reqListInvititations = nock('https://api.github.com', {
       reqheaders: {
-        authorization: `token ${githubToken}`,
-      },
+        authorization: `token ${githubToken}`
+      }
     })
       .get('/user/repository_invitations')
       .reply(200, [
         {
           id: invitationId,
           repository: {
-            full_name: `johndoe/anotherrepo`,
-          },
-        },
-      ]);
+            full_name: `johndoe/anotherrepo`
+          }
+        }
+      ])
 
     const reqAcceptInvitation = nock('https://api.github.com', {
       reqheaders: {
-        authorization: `token ${githubToken}`,
-      },
+        authorization: `token ${githubToken}`
+      }
     })
       .patch(`/user/repository_invitations/${invitationId}`)
-      .reply(204);
+      .reply(204)
 
-    expect.assertions(4);
+    expect.assertions(4)
 
     try {
-      await request('/v2/connect/johndoe/foobar');
+      await request('/v2/connect/johndoe/foobar')
     } catch (err) {
-      expect(reqListInvititations.isDone()).toBe(true);
-      expect(reqAcceptInvitation.isDone()).toBe(false);
-      expect(err.response.body).toBe('Invitation not found');
-      expect(err.statusCode).toBe(404);
+        expect(reqListInvititations.isDone()).toBe(true)
+        expect(reqAcceptInvitation.isDone()).toBe(false)
+        expect(err.response.body).toBe('Invitation not found')
+        expect(err.statusCode).toBe(404)
     }
-  });
-});
+  })
+})
 
 describe('Entry endpoint', () => {
   test('outputs a RECAPTCHA_CONFIG_MISMATCH error if reCaptcha options do not match (wrong site key)', async () => {
-    const data = {
-      ...helpers.getParameters(),
-      path: 'staticman.yml',
-    };
-    const reCaptchaSecret = helpers.encrypt('Some little secret');
-    const mockConfig = sampleData.config1.replace('@reCaptchaSecret@', reCaptchaSecret);
+    const data = Object.assign({}, helpers.getParameters(), {
+      path: 'staticman.yml'
+    })
+    const reCaptchaSecret = helpers.encrypt('Some little secret')
+    const mockConfig = sampleData.config1
+      .replace('@reCaptchaSecret@', reCaptchaSecret)
 
     nock('https://api.github.com', {
       reqheaders: {
-        Authorization: `token ${githubToken}`,
-      },
+        Authorization: `token ${githubToken}`
+      }
     })
       .get(`/repos/${data.username}/${data.repository}/contents/${data.path}?ref=${data.branch}`)
       .reply(200, {
@@ -122,26 +120,24 @@ describe('Entry endpoint', () => {
         content: btoa(mockConfig),
         sha: '3d21ec53a331a6f037a91c368710b99387d012c1',
         url: 'https://api.github.com/repos/octokit/octokit.rb/contents/staticman.yml',
-        git_url:
-          'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+        git_url: 'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
         html_url: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml',
         download_url: 'https://raw.githubusercontent.com/octokit/octokit.rb/master/staticman.yml',
         _links: {
-          git:
-            'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+          git: 'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
           self: 'https://api.github.com/repos/octokit/octokit.rb/contents/staticman.yml',
-          html: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml',
-        },
-      });
+          html: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml'
+        }
+      })
 
     const form = {
       'fields[name]': 'Eduardo Boucas',
       'options[reCaptcha][siteKey]': 'wrongSiteKey',
-      'options[reCaptcha][secret]': reCaptchaSecret,
-    };
-    const formData = querystring.stringify(form);
+      'options[reCaptcha][secret]': reCaptchaSecret
+    }
+    const formData = querystring.stringify(form)
 
-    expect.assertions(3);
+    expect.assertions(3)
 
     try {
       await request({
@@ -149,33 +145,30 @@ describe('Entry endpoint', () => {
         method: 'POST',
         uri: `/v2/entry/${data.username}/${data.repository}/${data.branch}/${data.property}`,
         headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-      });
-    } catch (response) {
-      const error = JSON.parse(response.error);
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      })
+    } catch(response) {
+      const error = JSON.parse(response.error)
 
-      expect(error.success).toBe(false);
-      expect(error.errorCode).toBe('RECAPTCHA_CONFIG_MISMATCH');
-      expect(error.message).toBe('reCAPTCHA options do not match Staticman config');
+      expect(error.success).toBe(false)
+      expect(error.errorCode).toBe('RECAPTCHA_CONFIG_MISMATCH')
+      expect(error.message).toBe('reCAPTCHA options do not match Staticman config')
     }
-  });
+  })
 
   test('outputs a RECAPTCHA_CONFIG_MISMATCH error if reCaptcha options do not match (wrong secret)', async () => {
-    const data = {
-      ...helpers.getParameters(),
-      path: 'staticman.yml',
-    };
-    const reCaptchaSecret = 'Some little secret';
-    const mockConfig = sampleData.config1.replace(
-      '@reCaptchaSecret@',
-      helpers.encrypt(reCaptchaSecret)
-    );
+    const data = Object.assign({}, helpers.getParameters(), {
+      path: 'staticman.yml'
+    })
+    const reCaptchaSecret = 'Some little secret'
+    const mockConfig = sampleData.config1
+      .replace('@reCaptchaSecret@', helpers.encrypt(reCaptchaSecret))
 
     nock('https://api.github.com', {
       reqHeaders: {
-        Authorization: `token ${githubToken}`,
-      },
+        Authorization: `token ${githubToken}`
+      }
     })
       .get(`/repos/${data.username}/${data.repository}/contents/${data.path}?ref=${data.branch}`)
       .reply(200, {
@@ -187,26 +180,24 @@ describe('Entry endpoint', () => {
         content: btoa(mockConfig),
         sha: '3d21ec53a331a6f037a91c368710b99387d012c1',
         url: 'https://api.github.com/repos/octokit/octokit.rb/contents/staticman.yml',
-        git_url:
-          'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+        git_url: 'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
         html_url: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml',
         download_url: 'https://raw.githubusercontent.com/octokit/octokit.rb/master/staticman.yml',
         _links: {
-          git:
-            'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+          git: 'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
           self: 'https://api.github.com/repos/octokit/octokit.rb/contents/staticman.yml',
-          html: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml',
-        },
-      });
+          html: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml'
+        }
+      })
 
     const form = {
       'fields[name]': 'Eduardo Boucas',
       'options[reCaptcha][siteKey]': '123456789',
-      'options[reCaptcha][secret]': 'foo',
-    };
-    const formData = querystring.stringify(form);
+      'options[reCaptcha][secret]': 'foo'
+    }
+    const formData = querystring.stringify(form)
 
-    expect.assertions(3);
+    expect.assertions(3)
 
     try {
       await request({
@@ -214,28 +205,27 @@ describe('Entry endpoint', () => {
         method: 'POST',
         uri: '/v2/entry/johndoe/foobar/master/comments',
         headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-      });
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      })
     } catch (response) {
-      const error = JSON.parse(response.error);
+      const error = JSON.parse(response.error)
 
-      expect(error.success).toBe(false);
-      expect(error.errorCode).toBe('RECAPTCHA_CONFIG_MISMATCH');
-      expect(error.message).toBe('reCAPTCHA options do not match Staticman config');
+      expect(error.success).toBe(false)
+      expect(error.errorCode).toBe('RECAPTCHA_CONFIG_MISMATCH')
+      expect(error.message).toBe('reCAPTCHA options do not match Staticman config')
     }
-  });
+  })
 
   test('outputs a PARSING_ERROR error if the site config is malformed', async () => {
-    const data = {
-      ...helpers.getParameters(),
-      path: 'staticman.yml',
-    };
+    const data = Object.assign({}, helpers.getParameters(), {
+      path: 'staticman.yml'
+    })
 
     const mockGetConfig = nock('https://api.github.com', {
       reqheaders: {
-        Authorization: `token ${githubToken}`,
-      },
+        Authorization: `token ${githubToken}`
+      }
     })
       .get(`/repos/${data.username}/${data.repository}/contents/${data.path}?ref=${data.branch}`)
       .reply(200, {
@@ -247,24 +237,22 @@ describe('Entry endpoint', () => {
         content: btoa(sampleData.config3),
         sha: '3d21ec53a331a6f037a91c368710b99387d012c1',
         url: 'https://api.github.com/repos/octokit/octokit.rb/contents/staticman.yml',
-        git_url:
-          'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+        git_url: 'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
         html_url: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml',
         download_url: 'https://raw.githubusercontent.com/octokit/octokit.rb/master/staticman.yml',
         _links: {
-          git:
-            'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
+          git: 'https://api.github.com/repos/octokit/octokit.rb/git/blobs/3d21ec53a331a6f037a91c368710b99387d012c1',
           self: 'https://api.github.com/repos/octokit/octokit.rb/contents/staticman.yml',
-          html: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml',
-        },
-      });
+          html: 'https://github.com/octokit/octokit.rb/blob/master/staticman.yml'
+        }
+      })
 
     const form = {
-      'fields[name]': 'Eduardo Boucas',
-    };
-    const formData = querystring.stringify(form);
+      'fields[name]': 'Eduardo Boucas'
+    }
+    const formData = querystring.stringify(form)
 
-    expect.assertions(5);
+    expect.assertions(4)
 
     try {
       await request({
@@ -272,17 +260,16 @@ describe('Entry endpoint', () => {
         method: 'POST',
         uri: '/v2/entry/johndoe/foobar/master/comments',
         headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-      });
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      })
     } catch (response) {
-      const error = JSON.parse(response.error);
+      const error = JSON.parse(response.error)
 
-      expect(error.success).toBe(false);
-      expect(error.errorCode).toBe('PARSING_ERROR');
-      expect(error.message).toBe('Error whilst parsing config file');
-      expect(error.rawError).toBeDefined();
-      expect(mockGetConfig.isDone()).toBe(true);
+      expect(error.success).toBe(false)
+      expect(error.errorCode).toBe('PARSING_ERROR')
+      expect(error.message).toBe('Error whilst parsing config file')
+      expect(error.rawError).toBeDefined()
     }
-  });
-});
+  })
+})
